9bee633a9de8b7b512a6b57615faa599a56ca1f2
Bibekad24 <166050249+Bibekad24@users.noreply.github.com>
2025-03-03 22:18:05 -0800

    Additional Comments

--- RR_final.c
+++ RR_final.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Structure to store process details
 typedef struct {
-    int process_id;
-    int arrival_time;
-    int burst_time;
-    int remaining_time;
-    int waiting_time;
-    int turnaround_time;
+    int process_id;      // Process ID
+    int arrival_time;    // Arrival time of process
+    int burst_time;      // Original burst time
+    int remaining_time;  // Remaining burst time (for round-robin execution)
+    int waiting_time;    // Time process waits in ready queue
+    int turnaround_time; // Total time from arrival to completion
 } Process;
 
-// Round Robin Scheduling
+// Simulates Round Robin Scheduling
 void roundRobin(Process proc[], int n, int quantum) {
     int time = 0, done;
-    
+
+    // Initialize remaining time for all processes
     for (int i = 0; i < n; i++) proc[i].remaining_time = proc[i].burst_time;
 
     printf("Execution Order: ");
@@ -21,12 +23,14 @@ void roundRobin(Process proc[], int n, int quantum) {
         done = 1;
         for (int i = 0; i < n; i++) {
             if (proc[i].remaining_time > 0) {
-                done = 0;
+                done = 0;  // Mark that at least one process is not done
                 printf("P%d ", proc[i].process_id);
+
+                // If process has more than quantum time left, execute for quantum time
                 if (proc[i].remaining_time > quantum) {
                     time += quantum;
                     proc[i].remaining_time -= quantum;
-                } else {
+                } else { // If remaining time is less than quantum, execute fully
                     time += proc[i].remaining_time;
                     proc[i].waiting_time = time - proc[i].arrival_time - proc[i].burst_time;
                     proc[i].remaining_time = 0;
@@ -34,12 +38,12 @@ void roundRobin(Process proc[], int n, int quantum) {
                 }
             }
         }
-        if (done) break;
+        if (done) break; // Exit when all processes are completed
     }
     printf("\n");
 }
 
-// Print process details
+// Prints process table with details
 void printProcesses(Process proc[], int n) {
     printf("Process ID\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\n");
     for (int i = 0; i < n; i++) {
@@ -50,9 +54,10 @@ void printProcesses(Process proc[], int n) {
 }
 
 int main() {
+    // Define process list with ID, arrival time, and burst time
     Process proc[] = {{1, 0, 24}, {2, 0, 3}, {3, 0, 3}};
     int n = sizeof(proc) / sizeof(proc[0]);
-    int quantum = 4;
+    int quantum = 4; // Time quantum for Round Robin scheduling
 
     roundRobin(proc, n, quantum);
     printProcesses(proc, n);
